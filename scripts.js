ec0831a26858f9d8e2b9e8cda79ed5e0dad50de4
Matt Kraatz <matt.kraatz@gmail.com>
2016-07-21 14:18:16 -0500

    wrapped function in a translator object

--- scripts.js
+++ scripts.js
@@ -1,24 +1,24 @@
-submit.addEventListener("click", main)
-
-function main() {
-  var inputEnglish = inputText.value.toLowerCase()
-  var inputWords = inputEnglish.split(" ");
-  var translatedWords = [];
-
-  var pigLatin = {
-    merry: "erry-May",
-    christmas: "istmas-Chray",
-    happy: "appy-Hay",
-    new: "ew-Nay",
-    year: "ear-Yay",
-    and: "and-way"
+var translator = (function() {
+  return {
+    getPigLatin: function() {
+      var inputWords = inputText.value.toLowerCase().split(" ")
+      var translatedWords = [];
+      var pigLatin = {
+        merry: "erry-May",
+        christmas: "istmas-Chray",
+        happy: "appy-Hay",
+        new: "ew-Nay",
+        year: "ear-Yay",
+        and: "and-way"
+      }
+      console.log(pigLatin.merry)
+      for (var i = 0; i < inputWords.length; i++) {
+        translatedWords.push(pigLatin[inputWords[i]]);
+      }
+      translation.innerHTML = translatedWords.join(" ");
+    }
   }
 
-  for (var i = 0; i < inputWords.length; i++) {
-    translatedWords.push(pigLatin[inputWords[i]]);
-  }
+    })()
 
-  var translatedPhrase = translatedWords.join(" ");
-
-  console.log(translatedPhrase)
-}
+submit.addEventListener("click", translator.getPigLatin)
