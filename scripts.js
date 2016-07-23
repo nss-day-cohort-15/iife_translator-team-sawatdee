872e6eaf76be72e0de9003833b954b13a68c155f
Matt Kraatz <matt.kraatz@gmail.com>
2016-07-23 11:25:45 -0500

    polishing

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
 var Translator = (function(translator){
   translator.translate = function() {
-    console.log("working")
     var inputWords = inputText.value.toLowerCase().split(" ")
     var translatedWords = [];
     var language = translator[select.value]
     for (var i = 0; i < inputWords.length; i++) {
-      translatedWords.push(language[inputWords[i]]);
+      if (language[inputWords[i]]) {
+        translatedWords.push(language[inputWords[i]]);
+      } else {
+        translatedWords.push(inputWords[i]);
+      }
     }
-    console.log(translatedWords)
     translation.innerHTML = translatedWords.join(" ");
   }
-
   submit.addEventListener("click", translator.translate)
   document.addEventListener("keypress", checkKey)
   function checkKey(e) {
